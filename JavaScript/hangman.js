08d5984280462c8d2680e3e6a531a9967f3ab2c0
EternalVigil <theeternalvigil@gmail.com>
2016-10-10 20:27:57 -0700

    javascript additions

--- JavaScript/hangman.js
+++ JavaScript/hangman.js
@@ -1,38 +1,53 @@
 // JavaScript Document
 var userInput = "";
 var wrongGuessNum = 0;
-var wordBank = ["monkeys"];
+var wordBank = ["blueberry"];
 var correctGuesses = [];
 var wrongGuesses = [];
 var hiddenWord = [];
 var guessRight = false;
+var wrongCount = 0;
+var playAgain = false;
 
 //after word selection, create equal length string of dashes for document output.
-for (var i = 0; i < wordBank[0].length; i++){
-hiddenWord[i] = "-";
+for (var i = 0; i < wordBank[0].length; i++) {
+	hiddenWord[i] = "-";
 }
 console.log(wordBank[0]);
 console.log(hiddenWord);
 
 
-document.onkeyup = function(event){
+document.onkeyup = function (event) {
 	userInput = String.fromCharCode(event.keyCode).toLowerCase();
 	console.log("User chose the letter: " + userInput);
-	for (i = 0; i < wordBank[0].length; i++){
-		if (userInput === wordBank[0].charAt(i)){
-			hiddenWord[i] = userInput;
-			guessRight = true;
-			console.log("You guessed right. There is a(n) " + userInput + "at location " + hiddenWord[i]);
-			correctGuesses.push(userInput);
-		
+	for (i = 0; i < correctGuesses.length; i++) {
+		if (correctGuesses[i] === userInput) {
+			alert("You already guessed that letter; Try again");
+		} else {
+			for (i = 0; i < wordBank[0].length; i++) {
+				if (userInput === wordBank[0].charAt(i)) {
+					hiddenWord[i] = userInput;
+					console.log(hiddenWord);
+					guessRight = true;
+					console.log("You guessed right. There is a(n) " + userInput + " at location " + i);
+					correctGuesses.push(userInput);
+				} else {
+					wrongCount += 1;
+				}
 			}
-		else{
-			guessRight = false;
+			if (wrongCount === hiddenWord.length) {
+				wrongGuesses.push = userInput;
+				wrongGuessNum += 1;
+			}
 		}
 	}
-	if (guessRight === false){
-		wrongGuesses.push = userInput;
-		console.log("Sorry, there isn't a(n) " + userInput);
-		wrongGuessNum += 1;
+
+
+	if (correctGuesses.length === hiddenWord.length) {
+		playAgain = confirm("You guessed right! Play again?");
 	}
+	if (wrongGuessNum === 7) {
+		playAgain = confirm("You have lost.  Play again?");
+	}
+
 };
