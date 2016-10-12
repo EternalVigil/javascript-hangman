06cbe42ca0e611f19c225733cb7a0667bdd10bff
Brandon Vigil <brandonvigil@Brandons-MacBook-Pro.local>
2016-10-11 18:53:19 -0700

    sorting code to functions

--- JavaScript/hangman.js
+++ JavaScript/hangman.js
@@ -1,53 +1,84 @@
 // JavaScript Document
 var userInput = "";
-var wrongGuessNum = 0;
-var wordBank = ["blueberry"];
-var correctGuesses = [];
+var guessLeft = 7;
+var wordBank = ["blueberry", "cherry", "orange", "apple", "grape"];
+var correctGuesses = [""];
 var wrongGuesses = [];
 var hiddenWord = [];
 var guessRight = false;
 var wrongCount = 0;
 var playAgain = false;
+var randomNum = 0;
+var theWord = [];
 
-//after word selection, create equal length string of dashes for document output.
-for (var i = 0; i < wordBank[0].length; i++) {
-	hiddenWord[i] = "-";
+function getRandom(arrLength) {
+	return Math.floor((Math.random() * arrLength) + 1);
 }
-console.log(wordBank[0]);
+
+function fillArray(arr) {
+	for (var i = 0; i < arr.length; i++) {
+		arr[i] = "-";
+		document.getElementById("wordBox").innerHTML += hiddenWord[i];
+	}
+}
+
+randomNum = getRandom(wordBank.length);
+
+theWord = wordBank[randomNum];
+console.log("The word you're getting now is at index: " + randomNum);
+
+console.log(theWord);
 console.log(hiddenWord);
 
+//after word selection, create equal length string of dashes for document output.
+fillArray(theWord);
+
+document.getElementById("wordBox").innerHTML = hiddenWord;
+document.getElementById("wrongLetters").innerHTML = wrongGuesses;
+document.getElementById("numAttempts").innerHTML = guessLeft;
 
 document.onkeyup = function (event) {
 	userInput = String.fromCharCode(event.keyCode).toLowerCase();
 	console.log("User chose the letter: " + userInput);
+
 	for (var j = 0; j < correctGuesses.length; j++) {
-		if (correctGuesses[j] === userInput) {
-			alert("You already guessed that letter; Try again");
+		//check against letters you have already guessed
+		if (userInput === correctGuesses[j]) {
+			console.log("You already guessed that letter; Try again");
 		} else {
-			for (var k = 0; k < wordBank[0].length; k++) {
-				if (userInput === wordBank[0].charAt(k)) {
+
+			console.log("You haven't guessed " + userInput + " yet.");
+
+			for (var k = 0; k < theWord.length; k++) {
+				//compare userInput to every index of the chosen word
+				if (userInput === theWord.charAt(k)) {
 					hiddenWord[k] = userInput;
 					console.log(hiddenWord);
 					guessRight = true;
 					console.log("You guessed right. There is a(n) " + userInput + " at location " + k);
+					document.getElementById("wordBox").innerHTML = hiddenWord;
 					correctGuesses.push(userInput);
 				} else {
+					//if the index doesn't contain the guessed letter, count up
 					wrongCount += 1;
 				}
 			}
 			if (wrongCount === hiddenWord.length) {
+				//the userInput letter wasn't found in the chosen word
+				console.log("Sorry, there isn't a " + userInput + " in this word.");
 				wrongGuesses.push = userInput;
-				wrongGuessNum += 1;
+				guessLeft -= 1;
 			}
 		}
 	}
 
 
-	if (correctGuesses.length === hiddenWord.length) {
+	if (hiddenWord === theWord) {
 		playAgain = confirm("You guessed right! Play again?");
 	}
-	if (wrongGuessNum === 7) {
+	if (guessLeft === 0) {
 		playAgain = confirm("You have lost.  Play again?");
 	}
 
 };
+
